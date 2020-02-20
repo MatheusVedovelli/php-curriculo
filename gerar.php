33c8d1cd80313b5c9a0fac1d4c92569b78b3851d
matheusvedovelli <matheusvedovelli@hotmail.com>
2020-02-20 19:43:32 -0300

    aaaaaaa

--- gerar.php
+++ gerar.php
@@ -27,24 +27,64 @@ if($_POST)
                 margin: 0 auto;
                 margin-top: 5px;
             }
+
+            .contato{
+                display: block;
+                margin: 0 auto;
+                width: 20vw;
+                min-width: 304px;
+                align-items: center;
+                justify-content: center;
+                overflow: hidden;
+            }
+    
+            .contato > input{
+                display: block;
+                width: 99%;
+                float: left;
+                clear: both;
+            }
+
+            .container{
+                width: 100vw;
+                max-width: 500px;
+                margin: 0 auto;
+            }
         </style>
     </head>
     <body>
-    <h1 class="text-center">'.$_POST['nome'].'</h1>
-    <h2 class="text-center">'.$_POST['profissao'].'</h3>
-    <hr>
-    <h3 class="text-center">Objetivo</h3>
-    <p class="text-center">'.$_POST['objetivo'].'</p>
-    <hr>
-    <h3 class="text-center">Experiencias</h3>
-    <p class="text-center">'.$_POST['experiencia'].'</p>
-    <hr>
-    <h3 class="text-center">Habilidades</h3>
-    <p class="text-center">'.$_POST['habilidade'].'</p>
+    <div class="container">
+        <h1 class="text-center">'.$_POST['nome'].'</h1>
+        <h2 class="text-center">'.$_POST['profissao'].'</h3>
+        <hr>
+        <h3 class="text-center">Objetivo</h3>
+        <p class="text-center">'.$_POST['objetivo'].'</p>
+        <hr>
+        <h3 class="text-center">Experiencias</h3>
+        <p class="text-center">'.$_POST['experiencia'].'</p>
+        <hr>
+        <h3 class="text-center">Habilidades</h3>
+        <p class="text-center">'.$_POST['habilidade'].'</p>
+        <hr>
+        <div class="contato">
+            <p><b>Email:</b> 
+            '.$_POST['email'].'</p>
+            <p><b>Telefone:</b> 
+            '.$_POST['telefone'].'</p>
+            <p><b>Site:</b> 
+            '.$_POST['site'].'</p>
+            <p><b>Endereço:</b> 
+            '.$_POST['endereco'].'</p>
+        </div>
+    </div>
     </body>
     </html>';
 
     echo $curriculo;
+
+    $arquivo = fopen($_POST['nome'].'.html', 'w+');
+    fwrite($arquivo, $curriculo);
+    fclose($arquivo);
 }
 
 ?>
