fb905140397009a50fff323a3242b002938f059c
Matheus Vedovelli <matheusvedovelli@hotmail.com>
2020-03-10 01:34:41 -0300

    alteraçao

--- gerar.php
+++ gerar.php
@@ -40,7 +40,7 @@ $curriculo = '
             <div class="col-md-12">
                 <h2>OBJETIVO</h2>
                 <div class="row">
-                    <div class="col-md-11 offset-md-1">
+                    <div class="col-md-10 offset-md-1">
                         '.$_POST["objetivo"].'
                     </div>
                 </div>
@@ -53,7 +53,7 @@ $curriculo = '
 
                     for($i = 0; $i < sizeof($experiences); $i++)
                     {
-                        $curriculo .=  '<div class="col-md-11 offset-md-1">
+                        $curriculo .=  '<div class="col-md-10 offset-md-1">
                                             <div class="exp0">
                                                 <b>DE '.$experiences[$i]["startdate"].' ATÉ '.$experiences[$i]["enddate"].'</b><br>
                                                 <div style="font-size: 20px;"><b style="color: rgb(0, 84, 240)">'.$experiences[$i]["cargo"].',</b> '.$experiences[$i]["empresa"].'</div>
@@ -74,7 +74,7 @@ $curriculo = '
 
                     for($i = 0; $i < sizeof($formations); $i++)
                     {
-                        $curriculo .=  '<div class="col-md-11 offset-md-1">
+                        $curriculo .=  '<div class="col-md-10 offset-md-1">
                                             <b>'.$formations[$i]["completedate"].'</b><br>
                                             <div style="font-size: 20px;"><b style="color: rgb(0, 84, 240)">'.$formations[$i]["graduacao"].',</b> '.$formations[$i]["instituicao"].'</div>
                                             <div class="text-muted">
@@ -106,7 +106,7 @@ $curriculo = '
             <div class="col-md-12 mt-3 mb-4">
                 <h2>PERFIL PROFISSIONAL</h2>
                 <div class="row">
-                    <div class="col-md-11 offset-md-1">
+                    <div class="col-md-10 offset-md-1">
                         '.$_POST["perfil"].'
                     </div>
                 </div>
